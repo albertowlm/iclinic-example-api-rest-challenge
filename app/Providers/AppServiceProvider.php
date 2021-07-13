5652d46b32a67a263626a01e0dbf06bb7dc79a53
Alberto Martins <amartins@medicinaesolutions.com>
2021-07-13 13:12:16 -0300

    Jesus cristo, agora vai

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        if(env('APP_ENV') == 'local'){
+        URL::forceScheme('https');
+        }
         $this->app->register(\L5Swagger\L5SwaggerServiceProvider::class);
 
     }
